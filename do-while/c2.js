9eb39522d61fe2d419205e717ab9ddec9868b39b
yudhies2109 <ma.yudhistira@hotmail.com>
2025-10-12 22:28:08 +0700

    Update C2 Do-While

--- do-while/c2.js
+++ do-while/c2.js
@@ -14,28 +14,35 @@ do {
 
   // Masukkan Pilihan dalam prompt
 
-  pilihan = prompt("Masukkan Angka :");
+  pilihan = parseInt(prompt("Pilih Menu :"));
 
-  // Cek apakah yang dimasukkan angka/bukan
-
-  if (isNaN(pilihan) || pilihan.trim() === "") {
-    console.log("Masukkan Angka!");
-    continue;
-  }
-
-  pilihan = Number(pilihan);
-
-//   Switch - Case
-
-switch (pilihan){
-    case 1 :
-        console.log(`Saldo Anda : ${saldo}`);
+  switch (pilihan) {
+    case 1:
+      console.log(`Saldo Anda : ${saldo}`);
+      lanjut = prompt("Apakah ingin melanjutkan? (y/n) :");
+      break;
+    case 2:
+      jml_penarikan = parseInt(prompt("Masukkan Jumlah Penarikan: "));
+      if (jml_penarikan >= saldo) {
+        console.log("Saldo Tidak Mecukupi!");
         lanjut = prompt("Apakah ingin melanjutkan? (y/n) :");
-        break;
+      } else {
+        console.log(`Penarikan berhasil. Saldo: ${saldo - jml_penarikan}`);
+        lanjut = prompt("Apakah ingin melanjutkan? (y/n) :");
+      }
+      break;
+    case 3:
+      jml_setoran = parseInt(prompt("Masukkan Jumlah Setoran :"));
+      console.log(`Setoran Berhasil. Saldo : ${saldo + jml_setoran}`);
+      lanjut = prompt("Apakah ingin melanjutkan? (y/n) :");
+      break;
+    case 4:
+      lanjut = "n";
+      break;
     default:
-        console.log("Pilihan Tidak Valid!")
-}
+      console.log("Pilihan Tidak Valid!");
+      lanjut = prompt("Apakah ingin melanjutkan? (y/n) :");
+  }
 } while (lanjut.toLowerCase() === "y");
-// } while (lanjut.toLowerCase()=== "y");
 
 console.log("Terima kasih telah menggunakan ATM!");
