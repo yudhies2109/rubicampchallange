1a39d40931b8529fa4b9bd59f77b73d71b5ba334
yudhies2109 <ma.yudhistira@hotmail.com>
2025-10-13 13:13:26 +0700

    update do while c7

--- do-while/c7.js
+++ do-while/c7.js
@@ -4,3 +4,20 @@ let tahap = ["Mengisi air", "Mencuci", "Membilas", "Mengeringkan"];
 let tambahBilas;
 let siklusSelesai = 0;
 
+function siklusPencucian() {
+  siklusSelesai++;
+  console.log(`=== SIKLUS PENCUCIAN ${siklusSelesai} ===`);
+  
+  for (let i = 0; i < tahap.length; i++) {
+    console.log(`${tahap[i]}...selesai`);
+  }
+
+}
+
+do{
+    siklusPencucian();
+    tambahBilas = prompt("Tambah siklus pembilasan? (y/n) :");
+
+}while(tambahBilas.toLowerCase() === "y")
+
+console.log(`Mesin cuci selesai! Total siklus: ${siklusSelesai}`);
