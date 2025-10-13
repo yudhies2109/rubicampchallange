c05e572640a6808f8fb8bb90379abb6c64a93e7f
yudhies2109 <ma.yudhistira@hotmail.com>
2025-10-13 09:35:54 +0700

    update do while c5

---
+++ do-while/c5.js
@@ -0,0 +1,22 @@
+const prompt = require("prompt-sync")();
+let angkaRandom;
+let percobaan = 1;
+
+function randomValue() {
+  return Math.floor(Math.random() * 100 + 1);
+}
+
+// console.log(randomValue());
+
+do {
+  angkaRandom = randomValue();
+
+  if (angkaRandom % 2 === 0) {
+    console.log(`Angka genap ditemukan: ${angkaRandom} dalam ${percobaan} percobaan`);
+    break;
+  } else {
+    console.log(`Percobaan ${percobaan}: ${angkaRandom} (ganjil, coba lagi)`);
+    percobaan++;
+  }
+
+} while (percobaan <= 100);
