a550c70156bf0ff885a8a3e21b4ff9654d978ef3
yudhies2109 <ma.yudhistira@hotmail.com>
2025-10-21 10:13:55 +0700

    updata c4 while

---
+++ while/c4.js
@@ -0,0 +1,18 @@
+let kata = "programming";
+let ditemukan = false;
+let i = 0;
+
+while (!ditemukan) {
+    for(; i < kata.length; i++){
+
+        // let cekKata = kata[i];
+    
+        if (kata[i] == "a") {
+            console.log("Huruf 'a' ditemukan di posisi:", i + 1);
+            ditemukan = true;
+            break;
+        } else {
+            console.log("Huruf 'a' tidak ditemukan");
+        }
+    }
+}
