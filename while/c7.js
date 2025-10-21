10b3ce9cadb8d96830b0cd7b73719a2f59271241
yudhies2109 <ma.yudhistira@hotmail.com>
2025-10-21 11:34:02 +0700

    update c7 while

--- while/c7.js
+++ while/c7.js
@@ -1,2 +1,15 @@
 let waktu = 10;
 let tombolDitekan = false;
+
+while (waktu > 0 && !tombolDitekan) {
+    
+    if (waktu == 7) {
+        console.log(`Countdown: ${waktu}`)
+        console.log(`Timer dihentikan oleh user pada detik ke-${waktu}`)
+        tombolDitekan = true;
+    } else {
+        console.log(`Countdown: ${waktu}`)
+    }
+
+    waktu--;
+}
