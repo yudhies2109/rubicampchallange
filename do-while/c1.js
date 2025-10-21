9a4c8cc13f8cf515c4f885959098094e367bedc8
yudhies2109 <ma.yudhistira@hotmail.com>
2025-10-21 10:12:42 +0700

    c1 Update

--- do-while/c1.js
+++ do-while/c1.js
@@ -3,16 +3,16 @@ let angka;
 let lanjut;
 
 do {
-    // Validasi yang di input hanya berupa angka
+  // Validasi yang di input hanya berupa angka
   do {
     angka = prompt("Masukkan angka:");
     if (isNaN(angka) || angka.trim() === "") {
       console.log("Masukkan Angka!");
     } else {
-        angka = Number(angka);
-        // Number Js function global untuk memastikan angka bertype number
+      angka = Number(angka);
+      // Number Js function global untuk memastikan angka bertype number
       break;
-    //   Break : Perintah untuk menghentikan looping
+      //   Break : Perintah untuk menghentikan looping
     }
   } while (true);
 
