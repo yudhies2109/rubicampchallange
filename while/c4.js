6c37304af17851219b5462a786ec49fd1143ac90
yudhies2109 <ma.yudhistira@hotmail.com>
2025-10-21 16:00:36 +0700

    c4.js while update

--- while/c4.js
+++ while/c4.js
@@ -3,15 +3,13 @@ let ditemukan = false;
 let i = 0;
 
 while (!ditemukan) {
-    for(; i < kata.length; i++){
-
-    
-        if (kata[i] == "a") {
-            console.log("Huruf 'a' ditemukan di posisi:", i + 1);
-            ditemukan = true;
-            break;
-        } else {
-            console.log("Huruf 'a' tidak ditemukan");
-        }
+  for (; i < kata.length; i++) {
+    if (kata[i] == "a") {
+      console.log("Huruf 'a' ditemukan di posisi:", i + 1);
+      ditemukan = true;
+      break;
+    } else {
+      console.log("Huruf 'a' tidak ditemukan");
     }
+  }
 }
