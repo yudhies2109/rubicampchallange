88ab8ecc39e33217a9dd5aa1030b5c4de83cc0d5
yudhies2109 <ma.yudhistira@hotmail.com>
2025-10-21 14:56:28 +0700

    c9.js update while

---
+++ while/c9.js
@@ -0,0 +1,15 @@
+let angka = 2;
+let iterasi = 0;
+
+
+while (angka <= 1000) {
+
+    iterasi++;
+    console.log(`Iterasi ${iterasi}: ${angka}^2 = ${angka * angka}`)
+    angka = angka * angka;    
+    
+}
+
+
+console.log("Angka " + 2 + " harus dikuadratkan " + iterasi + " kali untuk melebihi 1000");
+console.log("Hasil akhir: " + angka);
