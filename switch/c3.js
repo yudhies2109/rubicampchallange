e9592a25f1f84b54742a40b7af7332c631d0bec7
yudhies2109 <ma.yudhistira@hotmail.com>
2025-10-22 15:25:12 +0700

    c3 switch updated

--- switch/c3.js
+++ switch/c3.js
@@ -1,2 +1,27 @@
-let grade = "B";
-// Tulis kode switch statement di sini
+let grade = "E";
+
+switch (grade) {
+    case "A":
+        console.log(`Grade ${grade}: Sangat Baik`)
+        break;
+
+    case "B":
+        console.log(`Grade ${grade}: Baik`)
+        break;
+
+    case "C":
+        console.log(`Grade ${grade}: Cukup`)
+        break;
+    
+    case "D":
+        console.log(`Grade ${grade}: Kurang`)
+        break;
+
+    case "E":
+        console.log(`Grade ${grade}: Sangat Kurang`)
+        break;
+
+    default:
+        console.log("Grade tidak valid.")
+        break;
+}
