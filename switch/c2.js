f9519cc588bda58c3797733938f26d75812bf246
yudhies2109 <ma.yudhistira@hotmail.com>
2025-10-22 11:20:18 +0700

    c2 switch uodate

---
+++ switch/c2.js
@@ -0,0 +1,29 @@
+let pilihan = 2;
+
+
+switch (pilihan) {
+  case 1:
+    console.log("Anda memilih: Nasi Goreng");
+    console.log("Harga: Rp 15000");
+    break;
+
+  case 2:
+    console.log("Anda memilih: Mie Ayam");
+    console.log("Harga: Rp 12000");
+    break;
+
+case 3:
+    console.log("Anda memilih: Bakso");
+    console.log("Harga: Rp 10000");
+    break;
+
+case 4:
+    console.log("Anda memilih: Es Teh");
+    console.log("Harga: Rp 5000");
+    break;
+
+
+  default:
+    console.log("Menu tidak tersedia");
+    break;
+}
