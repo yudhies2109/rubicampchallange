b47cf37530c07339cea1beaa17efde8048feac27
yudhies2109 <ma.yudhistira@hotmail.com>
2025-10-22 11:05:43 +0700

    c1 switch update

---
+++ switch/c1.js
@@ -0,0 +1,35 @@
+let nomorHari = 3;
+// Tulis kode switch statement di sini
+
+switch (nomorHari) {
+    case 1:
+        console.log(`Hari ke-${nomorHari}: Senin`)
+        break;
+
+    case 2:
+        console.log(`Hari ke-${nomorHari}: Selasa`)
+        break;
+
+    case 3:
+        console.log(`Hari ke-${nomorHari}: Rabu`)
+        break;
+
+    case 4:
+        console.log(`Hari ke-${nomorHari}: Kamis`)
+        break;
+
+    case 5:
+        console.log(`Hari ke-${nomorHari}: Jumat`)
+        break;
+
+    case 6:
+        console.log(`Hari ke-${nomorHari}: Sabtu`)
+        break;
+    
+    case 7:
+        console.log(`Hari ke-${nomorHari}: Minggu`)
+        break;
+        
+    default:
+        break;
+}
