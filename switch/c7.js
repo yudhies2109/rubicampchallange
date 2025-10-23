41d797ab712ec53227ebeb0460d692eb4a69b3ed
yudhies2109 <ma.yudhistira@hotmail.com>
2025-10-23 11:11:26 +0700

    c7.js switch update

---
+++ switch/c7.js
@@ -0,0 +1,24 @@
+let size = "S";
+// Tulis kode switch statement di sini
+
+switch (size) {
+  case "S":
+    console.log(`Size ${size}: Small (36-38)`);
+    break;
+
+  case "M":
+    console.log(`Size ${size}: Medium (40-42)`);
+    break;
+
+  case "L":
+    console.log(`Size ${size}: Large (44-46)`);
+    break;
+
+  case "XL":
+    console.log(`Size ${size}: Extra Large (48-50)`);
+    break;
+
+  default:
+    console.log("Size tidak tersedia");
+    break;
+}
