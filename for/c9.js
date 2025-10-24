11cafee5f9627044715b131b0f92f64c80d37047
yudhies2109 <ma.yudhistira@hotmail.com>
2025-10-24 10:17:18 +0700

    c9.js update for

---
+++ for/c9.js
@@ -0,0 +1,10 @@
+let angka = 5;
+let faktorial = 1;
+
+for (let i = angka; i >= 1; i--) {
+    faktorial *= i
+}
+
+console.log("Faktorial dari " + angka + " adalah: " + faktorial);
+
+// Faktorial dari 5 adalah: 120
