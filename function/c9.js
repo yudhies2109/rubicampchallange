5b96ff95a99e2ae930244d37dd879e8d775ae397
yudhies2109 <ma.yudhistira@hotmail.com>
2025-10-24 14:30:43 +0700

    c9.js function added

---
+++ function/c9.js
@@ -0,0 +1,8 @@
+// Tulis function processOrder di sini
+function processOrder(){
+    
+}
+// Panggil function dan gunakan return value
+let totalBayar1 = processOrder("Nasi Goreng", 2);
+let totalBayar2 = processOrder("Mie Ayam", 3);
+console.log("Total yang harus dibayar: Rp " + (totalBayar1 + totalBayar2));
