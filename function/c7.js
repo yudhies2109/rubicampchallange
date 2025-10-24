dfa0c111dcc2e0b267f7c22e15b1c4b25457c31e
yudhies2109 <ma.yudhistira@hotmail.com>
2025-10-24 14:28:27 +0700

    c7.js updated

--- function/c7.js
+++ function/c7.js
@@ -1,5 +1,9 @@
 // Tulis function createProfile di sini
+function createProfile(nama, status) {
+  let statusMember = status !== undefined ? status : "Member";
 
+  return `Profil: ${nama} (${statusMember})`;
+}
 // Panggil function dengan dan tanpa parameter kedua
 let profil1 = createProfile("Budi");
 let profil2 = createProfile("Sari", "Admin");
