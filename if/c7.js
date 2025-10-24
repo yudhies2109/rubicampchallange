be7852b7e14caa8aed0348ffb1101bc5e72d5597
yudhies2109 <ma.yudhistira@hotmail.com>
2025-10-24 11:33:38 +0700

    c7 added if

---
+++ if/c7.js
@@ -0,0 +1,18 @@
+let skor = 85;
+// Tulis kode if-else if-else statement di sini
+
+if (skor >= 90) {
+    console.log(`Skor: ${skor}`);
+    console.log("Grade: A")
+} else if (skor >= 80){
+    console.log(`Skor: ${skor}`);
+    console.log("Grade: B")
+} else if (skor >= 70){
+    console.log(`Skor: ${skor}`);
+    console.log("Grade: C")
+} else if (skor >= 60){
+    console.log(`Skor: ${skor}`);
+    console.log("Grade: D")
+} else {
+    console.log("Grade: E")
+}
