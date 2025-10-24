574272d2585e70e541d83b5194ce9d7f1ff6cf78
yudhies2109 <ma.yudhistira@hotmail.com>
2025-10-24 12:52:47 +0700

    c8.js if updated

--- if/c8.js
+++ if/c8.js
@@ -4,11 +4,29 @@ let tinggi = 1.75; // meter
 // Tulis kode if-else if-else statement di sini
 
 function hitungBMI (berat, tinggi){
-    let BMI = berat / tinggi;
-    console.log("Ini BMI", BMI)
+    let BMI = berat / Math.pow(tinggi, 2);
+    // console.log("Ini BMI", BMI)
     return BMI;
 }
 
+let nilaiBMI = hitungBMI(berat, tinggi);
+
+// console.log(nilaiBMI);
+
 // hitungBMI(berat, tinggi)
 
-console.log("Ini hasil BMI :", hitungBMI(berat, tinggi));
+// console.log("Ini hasil BMI :", hitungBMI(berat, tinggi));
+
+if (nilaiBMI < 18.5) {
+    console.log(`BMI: ${nilaiBMI}`);
+    console.log("Kategori: Underweight");
+} else if(nilaiBMI < 25){
+    console.log(`BMI: ${nilaiBMI}`);
+    console.log("Kategori: Normal");
+}else if(nilaiBMI < 30){
+    console.log(`BMI: ${nilaiBMI}`);
+    console.log("Kategori: Overweight");
+} else {
+    console.log(`BMI: ${nilaiBMI}`);
+    console.log("Kategori: Obese");
+}
