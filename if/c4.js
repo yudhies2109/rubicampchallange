c47d4f5a13d680d68d60d7412fd9503201c538ec
yudhies2109 <ma.yudhistira@hotmail.com>
2025-10-24 10:54:49 +0700

    c4.js if added

---
+++ if/c4.js
@@ -0,0 +1,8 @@
+let password = "admin12";
+// Tulis kode if-else statement di sini
+
+if (password === "admin123") {
+    console.log("Login berhasil! Selamat datang.")
+} else {
+    console.log("Password Salah! Silahkan Coba Kembali.")
+}
