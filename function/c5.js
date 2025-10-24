a3fd3804b6416a720dba6d0764158797042e5ef6
yudhies2109 <ma.yudhistira@hotmail.com>
2025-10-24 13:54:04 +0700

    c5.js function added

---
+++ function/c5.js
@@ -0,0 +1,12 @@
+// Tulis function displayUserInfo di sini
+function displayUserInfo(nama, umur, kota){
+    console.log("=== Info User ===");
+    console.log(`Nama: ${nama}`);
+    console.log(`Umur: ${umur} tahun`);
+    console.log(`Umur: ${kota} tahun`);
+}
+
+// Panggil function dengan data berbeda
+displayUserInfo("Ahmad", 25, "Jakarta");
+console.log(" ");
+displayUserInfo("Lisa", 30, "Bandung");
