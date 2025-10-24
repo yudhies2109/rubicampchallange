43ecdfa800d74251223493bdeec0f42c14db1610
yudhies2109 <ma.yudhistira@hotmail.com>
2025-10-24 13:45:29 +0700

    c4.js function updated

---
+++ function/c4.js
@@ -0,0 +1,10 @@
+function calculateSquare(angka) {
+    let kuadrat = Math.pow(angka, 2);
+
+    return kuadrat;
+}
+
+let hasil1 = calculateSquare(5);
+let hasil2 = calculateSquare(8);
+console.log("5 kuadrat = " + hasil1);
+console.log("8 kuadrat = " + hasil2);
