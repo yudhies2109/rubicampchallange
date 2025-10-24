eda0ce3ad0b9a8d9564a47dfcfa9e8710d791949
yudhies2109 <ma.yudhistira@hotmail.com>
2025-10-24 10:49:57 +0700

    c3.js if updated

---
+++ if/c3.js
@@ -0,0 +1,8 @@
+let angka = 10;
+// Tulis kode if-else statement di sini
+
+if (angka % 2 === 0) {
+    console.log(`${angka} adalah bilangan genap`)
+} else {
+    console.log(`${angka} adalah bilangan ganjil`)
+}
