5e9379ec2180cdb6241f432cebcc3c9e1ee3e5b3
yudhies2109 <ma.yudhistira@hotmail.com>
2025-10-24 13:57:42 +0700

    c7.js funcion added

---
+++ function/c7.js
@@ -0,0 +1,7 @@
+// Tulis function createProfile di sini
+
+// Panggil function dengan dan tanpa parameter kedua
+let profil1 = createProfile("Budi");
+let profil2 = createProfile("Sari", "Admin");
+console.log(profil1);
+console.log(profil2);
