6f0eca12be1146517cb7276d691097944ed5b5d0
yudhies2109 <ma.yudhistira@hotmail.com>
2025-10-24 10:20:25 +0700

    c11 updated for

---
+++ for/c11.js
@@ -0,0 +1,6 @@
+let buah = ["apel", "jeruk", "pisang", "mangga", "anggur"];
+// Tulis kode for loop di sini untuk menampilkan indeks dan nilai array
+
+for (let i = 0; i < buah.length; i++) {
+    console.log(`Indeks ${i} : ${buah[i]}`);
+}
