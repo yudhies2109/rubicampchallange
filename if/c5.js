61515ad501308f59d012f42956f081bb81fad07a
yudhies2109 <ma.yudhistira@hotmail.com>
2025-10-24 10:59:14 +0700

    c5.js if added

---
+++ if/c5.js
@@ -0,0 +1,9 @@
+let suhu = 50;
+
+if (suhu >= 35) {
+    console.log(`Suhu ${suhu}°C: Sangat Panas`)
+} else if (suhu >= 25 ) {
+    console.log(`Suhu ${suhu}°C: Panas`)
+} else if(suhu >= 15){
+    console.log(`Suhu ${suhu}°C: Hangat`)
+}
