f0e84ea0e987108b6a3881f79d4e2aee89c1288b
yudhies2109 <ma.yudhistira@hotmail.com>
2025-10-24 14:37:32 +0700

    c9.js function udpdate

--- function/c9.js
+++ function/c9.js
@@ -1,7 +1,19 @@
 // Tulis function processOrder di sini
-function processOrder(){
-    
+function processOrder(namaItem, jumlah){
+    let hargaItem = 15000;
+    let subTotal = jumlah * hargaItem;
+
+    console.log(" ");
+    console.log("=== Detail Pesanan ===");
+    console.log(`Item: ${namaItem}`);
+    console.log(`Jumlah: ${jumlah}`);
+    console.log(`Harga per item: Rp ${hargaItem}`);
+    console.log(`Sub Total: Rp ${subTotal}`);
+    console.log(" ");
+
+    return subTotal;
 }
+
 // Panggil function dan gunakan return value
 let totalBayar1 = processOrder("Nasi Goreng", 2);
 let totalBayar2 = processOrder("Mie Ayam", 3);
