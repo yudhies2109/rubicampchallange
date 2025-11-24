1ce03eba36d6689a2a19ebcdc2cddbbea16a7bd8
yudhies2109 <ma.yudhistira@hotmail.com>
2025-11-24 13:41:33 +0700

    edited c6 while

--- while/c6.js
+++ while/c6.js
@@ -5,19 +5,20 @@ let percobaan = 0;
 let maxPercobaan = 3;
 let berhasil = false;
 
-while (maxPercobaan >= 1 && maxPercobaan <= 3) {
+while (!berhasil) {
     password = prompt(`Input Password : `);
 
     maxPercobaan--;
 
     if (password === "admin123") {
         console.log(`Login berhasil!`);
-        break;
+        berhasil = true;
     } else {
         if (maxPercobaan > 0) {
             console.log(`Password salah. Sisa percobaan: ${maxPercobaan}`)
         } else {
             console.log("Akses ditolak. Terlalu banyak percobaan gagal.");
+            break;
         }
     }
 
