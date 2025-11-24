8a45d7eeff9c18e338b6559b9900f8c512c66282
yudhies2109 <ma.yudhistira@hotmail.com>
2025-11-24 13:43:38 +0700

    c5.js updated switch

--- switch/c5.js
+++ switch/c5.js
@@ -2,54 +2,53 @@ let angka1 = 15;
 let angka2 = 4;
 let operator = "%";
 
-function callMod(){
-  result = angka1 % angka2;
-  return result;
+function callMod(a, b) {
+    result = a % b;
+    return result;
 }
 
-function callAdd(){
-  result = angka1 + angka2;
-  return result;
+function callAdd(a, b) {
+    result = a + b;
+    return result;
 }
 
-function callSubstract(){
-  result = angka1 - angka2;
-  return result;
+function callSubstract(a, b) {
+    result = a - b;
+    return result;
 }
 
-function callMultiplication(){
-  result = angka1 * angka2;
-  return result;
+function callMultiplication(a, b) {
+    result = a * b;
+    return result;
 }
 
-function calDiv(){
-  result = angka1 / angka2;
-  return result;
+function calDiv(a, b) {
+    result = angka1 / angka2;
+    return result;
 }
 
 switch (operator) {
-  case"%":
-    console.log(`${angka1} % ${angka2} = ${callMod()}`);
-    break;
+    case "%":
+        console.log(`${angka1} % ${angka2} = ${callMod(angka1, angka2)}`);
+        break;
 
-  case"+":
-    console.log(`${angka1} + ${angka2} = ${callAdd()}`);
-    break;
+    case "+":
+        console.log(`${angka1} + ${angka2} = ${callAdd(angka1, angka2)}`);
+        break;
 
-  case"-":
-    console.log(`${angka1} - ${angka2} = ${callSubstract()}`);
-    break;
+    case "-":
+        console.log(`${angka1} - ${angka2} = ${callSubstract(angka1, angka2)}`);
+        break;
 
-  case"*":
-    console.log(`${angka1} * ${angka2} = ${callMultiplication()}`);
-    break;
+    case "*":
+        console.log(`${angka1} * ${angka2} = ${callMultiplication(angka1, angka2)}`);
+        break;
 
-  case"/":
-    console.log(`${angka1} / ${angka2} = ${calDiv()}`);
-    break;
+    case "/":
+        console.log(`${angka1} / ${angka2} = ${calDiv(angka1, angka2)}`);
+        break;
 
-  default:
-    console.log("operator tidak valid");
-    break;
+    default:
+        console.log("operator tidak valid");
+        break;
 }
-
