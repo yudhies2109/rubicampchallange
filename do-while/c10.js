a9c77533dfd41be2681c57c30cfc15d36faed080
yudhies2109 <ma.yudhistira@hotmail.com>
2025-11-30 14:37:29 +0700

    do while added update c10.js

--- do-while/c10.js
+++ do-while/c10.js
@@ -5,34 +5,34 @@ let nasabahDilayani = 0;
 let adaNasabahBaru;
 
 function layaniNasabah() {
-  let jmlNasabah = antrian.length;
+    let jmlNasabah = antrian.length;
 
-  for (let i = 0; i < jmlNasabah; i++) {
-    console.log(`Melayani nasabah: ${antrian[i]}`);
-    nasabahDilayani++;
+    for (let i = 0; i < jmlNasabah; i++) {
+        console.log(`Melayani nasabah: ${antrian[i]}`);
+        nasabahDilayani++;
 
-    if(jmlNasabah - i - 1 === 0){
-      console.log("Antrian Kosong")
-    }else {
-      console.log(`Sisa antrian: ${jmlNasabah - i - 1} orang`);
+        if (jmlNasabah - i - 1 === 0) {
+            console.log("Antrian Kosong")
+        } else {
+            console.log(`Sisa antrian: ${jmlNasabah - i - 1} orang`);
+        }
     }
-  }
 
-  antrian = [];
+    antrian = [];
 }
 
 
 do {
 
-  layaniNasabah();
+    layaniNasabah();
 
-  // console.log("Antrian Baru", antrian);
+    // console.log("Antrian Baru", antrian);
 
-  adaNasabahBaru = prompt("Ada nasabah baru yang datang? (y/n):")
-  if(adaNasabahBaru.toLowerCase() === "y"){
-    antrian.push("Dewi");
-    console.log(`Nasabah baru: ${antrian[0]}`);
-  }
+    adaNasabahBaru = prompt("Ada nasabah baru yang datang? (y/n):")
+    if (adaNasabahBaru.toLowerCase() === "y") {
+        antrian.push("Dewi");
+        console.log(`Nasabah baru: ${antrian[0]}`);
+    }
 
 } while (adaNasabahBaru.toLowerCase() === "y");
 
