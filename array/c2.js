312c2c6eec694442497f2a4d16b6e893ce155bac
yudhies2109 <ma.yudhistira@hotmail.com>
2025-12-05 09:48:56 +0700

    added array c2.js

--- array/c2.js
+++ array/c2.js
@@ -1,15 +1,15 @@
-function analyzeElements(arr){
+function analyzeElements(arr) {
     let resultAnalyze = [];
-      for (let i = 0; i < arr.length; i++) {
-          
-         resultAnalyze.push({
-              value: arr[i],
-              index: i,
-              type: typeof(arr[i])
-          })
-      }
+    for (let i = 0; i < arr.length; i++) {
+
+        resultAnalyze.push({
+            value: arr[i],
+            index: i,
+            type: typeof(arr[i])
+        })
+    }
     return resultAnalyze;
-  }
-  
-  console.log(analyzeElements([10, "hello", true]));
-  console.log(analyzeElements([1, 2, 3]));
+}
+
+console.log(analyzeElements([10, "hello", true]));
+console.log(analyzeElements([1, 2, 3]));
