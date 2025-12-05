7b6f55fc7de961329d366cb7707249f8c48f0f9d
yudhies2109 <ma.yudhistira@hotmail.com>
2025-12-05 09:58:54 +0700

    update c10.js function

--- function/c10.js
+++ function/c10.js
@@ -1,59 +1,35 @@
 // Tulis function validateAndCalculateGrade di sini
 function validateAndCalculateGrade(nama, nilai) {
-
     let grade = "";
-
     if (nilai >= 90 && nilai <= 100) {
-        console.log(" ");
-        console.log("=== Hasil Penilaian ===");
-        console.log(`Nama: ${nama}`);
-        console.log(`Nilai: ${nilai} (Valid)`);
-        console.log(`Grade: A`);
         grade = "A";
-    } else if(nilai >= 80 && nilai <= 89){
-        console.log(" ");
-        console.log("=== Hasil Penilaian ===");
-        console.log(`Nama: ${nama}`);
-        console.log(`Nilai: ${nilai} (Valid)`);
-        console.log(`Grade: B`);
+    } else if (nilai >= 80 && nilai <= 89) {
         grade = "B";
-    } else if(nilai >= 70 && nilai <= 79){
-        console.log(" ");
-        console.log("=== Hasil Penilaian ===");
-        console.log(`Nama: ${nama}`);
-        console.log(`Nilai: ${nilai} (Valid)`);
-        console.log(`Grade: C`);
+    } else if (nilai >= 70 && nilai <= 79) {
         grade = "C";
-    } else if(nilai >= 60 && nilai <= 69){
-        console.log(" ");
-        console.log("=== Hasil Penilaian ===");
-        console.log(`Nama: ${nama}`);
-        console.log(`Nilai: ${nilai} (Valid)`);
-        console.log(`Grade: D`);
+    } else if (nilai >= 60 && nilai <= 69) {
         grade = "D";
-    } else if(nilai >= 0 && nilai <= 59){
-        console.log(" ");
-        console.log("=== Hasil Penilaian ===");
-        console.log(`Nama: ${nama}`);
-        console.log(`Nilai: ${nilai} (Valid)`);
-        console.log(`Grade: D`);
+    } else if (nilai >= 0 && nilai <= 59) {
         grade = "E";
     } else {
-        console.log(" ");
-        console.log("=== Hasil Penilaian ===");
-        console.log(`Nama: ${nama}`);
-        console.log(`Nilai: ${nilai} (Tidak Valid - harus 0-100)`);
-
         grade = null;
     }
 
+    console.log("=== Hasil Penilaian === ");
+    console.log(`Nama : ${nama}`);
+    console.log(`Nilai : ${nilai} (${grade ? "Valid" : "Tidak Valid - harus 0 - 100"} )`);
+    if (grade)
+        console.log(`Grade: ${grade}`);
+    console.log();
+
     return grade;
 }
+
 // Test dengan berbagai nilai
 let grade1 = validateAndCalculateGrade("Budi", 85);
 let grade2 = validateAndCalculateGrade("Sari", 150); // invalid
 let grade3 = validateAndCalculateGrade("Andi", 72);
-console.log(" ");
+
 console.log("Grade Budi: " + grade1);
 console.log("Grade Sari: " + grade2);
 console.log("Grade Andi: " + grade3);
