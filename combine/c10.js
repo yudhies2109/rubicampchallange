ed0dd737a95328face8f5aa1a42f332d2e61100c
yudhies2109 <ma.yudhistira@hotmail.com>
2025-12-18 13:59:15 +0700

    c10.js added combine

---
+++ combine/c10.js
@@ -0,0 +1,10 @@
+function sumUntilLimit(arguments) {
+
+    console.log(arguments);
+
+}
+
+// Tulis function sumUntilLimit di sini
+
+console.log(sumUntilLimit(10));
+console.log(sumUntilLimit(20));
