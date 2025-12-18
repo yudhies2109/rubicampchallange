c829c4b0112b40a0e246220762a8fce1555ce6ac
yudhies2109 <ma.yudhistira@hotmail.com>
2025-12-18 13:42:34 +0700

    c2.js combine added

---
+++ combine/c2.js
@@ -0,0 +1,11 @@
+function printNumbers(n) {
+    for (let i = 1; i <= n; i++) {
+        console.log(i)
+    }
+}
+
+// Tulis function printNumbers di sini
+
+printNumbers(5);
+console.log("---");
+printNumbers(3);
