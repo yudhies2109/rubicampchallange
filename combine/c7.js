a871be9dd8fc9cdeaf0b6c86400042d5b8bd6de6
yudhies2109 <ma.yudhistira@hotmail.com>
2025-12-19 05:55:27 +0700

    combine c7.js added

--- combine/c7.js
+++ combine/c7.js
@@ -8,35 +8,34 @@ function calculateGrades(arr) {
     }
 
     for (let i = 0; i < arr.length; i++) {
-        let score = arr[i];
+        let score = arr[i]
 
         switch (true) {
-            case score >= 90:
-                result.A++;
-                break;
+            case score >= 86 && score <= 100:
+                result.A++
+                    break;
 
-            case score >= 75:
-                result.B++;
-                break;
+            case score >= 76 && score <= 85:
+                result.B++
+                    break;
 
-            case score >= 60:
-                result.C++;
-                break;
+            case score >= 66 && score <= 75:
+                result.C++
+                    break;
 
-            case score >= 50:
-                result.D++;
-                break;
+            case score >= 56 && score <= 65:
+                result.D++
+                    break;
 
             default:
                 result.E++;
         }
+
     }
-
     return result;
-
 }
 
 // Tulis function calculateGrades di sini
 
-let grades = calculateGrades([85, 92, 67, 78, 45, 88]);
+let grades = calculateGrades([85, 92, 62, 78, 45, 88]);
 console.log(grades);
