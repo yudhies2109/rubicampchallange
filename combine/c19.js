15afd27c827f1d6850aa4fa69582273ef71b5299
yudhies2109 <ma.yudhistira@hotmail.com>
2025-12-31 09:41:30 +0700

    combine c19.js updated

--- combine/c19.js
+++ combine/c19.js
@@ -1,41 +1,59 @@
 function ticTacToe(board) {
     let moves = 0;
-    let nilaiX = 0;
-    let nilaiO = 0;
+    let winner = '';
+    let winType = 0;
+    let gameOver = false;
 
 
     for (let i = 0; i < board.length; i++) {
         for (let j = 0; j < board.length; j++) {
-            // console.log(board[i][j]);
-
-            // Pisahkan mana yang nilai 'x' atau 'o'
-            if (board[i][j] == 'X') {
-                // console.log("Nilai Ini Adalah x :", board[i][j]);
-                nilaiX++;
-            } else {
-                // console.log("Nilai Ini Adalah o :", board[i][j]);
-                nilaiO++;
+            if (board[i][j].trim() !== '') {
+                moves++;
             }
 
-            moves++;
         }
     }
 
+
+    for (let i = 0; i < board[0].length; i++) {
+        // Cek Baris
+        if (board[i][0] === board[i][1] && board[i][1] === board[i][2]) {
+            winner = board[i][0];
+            winType = 'row';
+            gameOver = true;
+            // Cek Kolom
+        } else if (board[0][i] === board[1][i] && board[1][i] === board[2][i]) {
+            winner = board[0][i];
+            winType = 'column';
+            gameOver = true;
+        }
+    }
+
+    // Cek Diagonal
+
+    if (board[0][0] === board[1][1] && board[1][1] === board[2][2]) {
+        winner = board[0][0];
+        winType = 'diagonal';
+        gameOver = true;
+    } else if (board[0][2] === board[1][1] && board[1][1] === board[2][0]) {
+        winner = board[0][2];
+        winType = 'diagonal';
+        gameOver = true;
+    }
+
     return {
-        winner: "X",
-        nilaiX,
-        nilaiO,
-        winType: "diagonal",
-        gameOver: true,
-        moves: moves
+        winner,
+        winType,
+        gameOver,
+        moves
     }
 }
 
 // Tulis function ticTacToe di sini
 
 let board = [
-    ['X', 'O', 'X'],
-    ['O', 'X', 'O'],
-    ['X', 'O', 'X']
+    ['X', 'X', 'O'],
+    ['X', 'O', 'O'],
+    ['O', 'O', 'X']
 ];
 console.log(ticTacToe(board));
